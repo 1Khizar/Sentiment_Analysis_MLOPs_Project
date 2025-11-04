7fb0cbbf326a41c33f82d2c39fce4a5038dfabe8
1Khizar <khizar12006@gmail.com>
2025-11-04 17:14:36 +0500

    first commit

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from src.data_ingestion.data_ingestion import DataIngestion
+# from src.data_transformation import DataTransformation
+
+# if __name__=="__main__":
+#     di = DataIngestion(input_path="test_data/twitter_dataset.csv", output_path="Data/raw")
+#     data = di.read_data()
+#     di.save_raw_data(data)
+#     di.save_train_test_data(data)
+    
