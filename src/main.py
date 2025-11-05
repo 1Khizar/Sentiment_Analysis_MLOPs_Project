b6ca930d4a4de35370a71329f98f2c2f900d8b52
1Khizar <khizar12006@gmail.com>
2025-11-05 22:52:14 +0500

    Changes in data

--- src/main.py
+++ src/main.py
@@ -9,11 +9,11 @@ if __name__=="__main__":
     di.save_raw_data(data)
     di.save_train_test_data(data)
     
-    dt = DataTransformation(raw_file_path="Data/raw/raw_data.csv", output_file_path="Data/processed")
-    preprocessed_train_df, preprocessed_test_df=dt.preprocess_dataset()
-    dt.save_preprocessd_datasets(preprocessed_train_df, preprocessed_test_df)
+    # dt = DataTransformation(raw_file_path="Data/raw/raw_data.csv", output_file_path="Data/processed")
+    # preprocessed_train_df, preprocessed_test_df=dt.preprocess_dataset()
+    # dt.save_preprocessd_datasets(preprocessed_train_df, preprocessed_test_df)
 
-    mt = ModelTrainer(model_file_path="src/model",train_data="Data/processed/preprocess_train_data.csv", test_data="Data/processed/preprocess_test_data.csv")
-    acc=mt.initiate_model_training()
-    print("Last",acc)
+    # mt = ModelTrainer(model_file_path="src/model",train_data="Data/processed/preprocess_train_data.csv", test_data="Data/processed/preprocess_test_data.csv")
+    # acc=mt.initiate_model_training()
+    # print("Last",acc)
 
